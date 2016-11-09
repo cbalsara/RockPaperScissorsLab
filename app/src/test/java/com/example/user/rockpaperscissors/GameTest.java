289827c93652c4854e2a53d709f85bb71ce2e753
Cyrus <cdbalsara@icloud.com>
2016-11-09 14:10:10 +0000

    added more tests

--- app/src/test/java/com/example/user/rockpaperscissors/GameTest.java
+++ app/src/test/java/com/example/user/rockpaperscissors/GameTest.java
@@ -3,6 +3,7 @@ package com.example.user.rockpaperscissors;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 import static junit.framework.Assert.assertEquals;
@@ -14,20 +15,52 @@ import static junit.framework.Assert.assertEquals;
 public class GameTest {
 
     Game game;
+    ArrayList<String> TestMoves;
 
 
     @Before
     public void before() {
-        game = new Game("Rock");
+        TestMoves = new ArrayList<String>();
+        TestMoves.add("Rock");
+        TestMoves.add("Paper");
+        TestMoves.add("Scissors");
+//        game = new Game("Rock");
 
     }
 
     @Test
     public void canGetMove() {
+        Game game = new Game("Rock");
         assertEquals("Rock", game.getMove());
     }
 
+    @Test
+    public void createComputerMovesWithList() {
+        assertEquals(3, TestMoves.size());
+    }
 
+//
+//    @Test
+//    public void getAnswerTest() {
+//        Answers answers = new Answers(testAnswers);
+//
+//        String answer = answers.getAnswer();
+//
+//        System.out.println("The answer is... " + answer);
+//        assertNotNull(answer);
+//    }
+//
+//
+//    @Test
+//    public void addTest() {
+//        Answers answers = new Answers();
+//
+//        int originalNumberOfAnswers = answers.getLength();
+//        answers.add("Yes");
+//
+//        assertEquals(originalNumberOfAnswers + 1, answers.getLength());
+//    }
+//}
 
 
 
