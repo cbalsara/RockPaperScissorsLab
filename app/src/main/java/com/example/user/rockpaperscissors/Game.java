d2051409137adec0cc0f8aa6b7f9c97dd67d3fce
Cyrus <cdbalsara@icloud.com>
2016-11-09 13:35:09 +0000

    added rock test

--- app/src/main/java/com/example/user/rockpaperscissors/Game.java
+++ app/src/main/java/com/example/user/rockpaperscissors/Game.java
@@ -1,8 +1,52 @@
 package com.example.user.rockpaperscissors;
 
+import java.util.ArrayList;
+
 /**
  * Created by user on 09/11/2016.
  */
 
 public class Game {
-}
+
+    private String move;
+    protected ArrayList<String> mComputerMove;
+
+        public Game(String move){
+            this.move = move;
+            mComputerMove = new ArrayList<String>();
+            setUpComputerMoves();
+        }
+
+        public void setUpComputerMoves() {
+        String [] moves = {
+                "Rock",
+                "Paper",
+                "Scissors"
+        };
+        for (String move : moves) {
+            add(move);
+        }
+    }
+
+        public void add(String newMove) {
+            mComputerMove.add(newMove);
+        }
+
+        public String getMove() {
+            return this.move;
+        }
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
--- app/src/test/java/com/example/user/rockpaperscissors/GameTest.java
+++ app/src/test/java/com/example/user/rockpaperscissors/GameTest.java
@@ -1,8 +1,37 @@
 package com.example.user.rockpaperscissors;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static junit.framework.Assert.assertEquals;
+
 /**
  * Created by user on 09/11/2016.
  */
 
 public class GameTest {
+
+    Game game;
+
+
+    @Before
+    public void before() {
+        game = new Game("Rock");
+
+    }
+
+    @Test
+    public void canGetMove() {
+        assertEquals("Rock", game.getMove());
+    }
+
+
+
+
+
+
+
+
 }
